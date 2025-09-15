907e8c4e7bda83d68a44b34c79cbabb49bb6a4c2
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-15 12:41:47 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,20 +1,23 @@
 def max_of_two(x, y):
     """Given x and y, that are 2 numbers, return the biggest number."""
-    return max(x, y)
+    if x >= y:
+        return x
+    else:
+        y
 
 def max_of_three(x, y, z):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return max(x, y, z)
+    if x >= y and x >= z:
+        return x
+    elif y >= x and y >= z:
+        return y 
+    else:
+        return z
 
-result1 = max_of_two(5, 4)
-print(result1)   
-result2 = max_of_two(-2, -3) 
-print(result2)  
-result3 = max_of_two(0, 0)
-print(result3)    
-result4 = max_of_three(5, 4, 7)
-print(result4)  
-result5 = max_of_three(-2, -3, -1) 
-print(result5) 
-result6 = max_of_three(0, 0, 0)
-print(result6)  
+print(max_of_two(5,4))   
+print(max_of_two(-2,-3)) 
+print(max_of_two(0,0))   
+
+print(max_of_three(5,4,7))   
+print(max_of_three(-2,-3,-1)) 
+print(max_of_three(0,0,0))    
