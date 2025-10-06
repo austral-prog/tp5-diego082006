30bbff39180ca5aedd64d1666834616674d52ad5
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-10-06 12:17:59 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,10 +1,6 @@
 def number_to_month(month):
-    months = ["Janaury", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    months = ["enero", "febrero", "marzo", "abril", "mayo", "diciembre", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembrer"]
     if 1 <= month <= 12:
         return months[month - 1]
     else:
         return "error"
-
-print(number_to_month(1))
-print(number_to_month(12))
-print(number_to_month(99))
