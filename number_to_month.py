eeb56b2f1df8cec7bddca98c4e03e3512936bfb6
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-10-06 12:15:06 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,10 @@
-# Replace the "ANSWER HERE" for your answer
-
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    months = ["Janaury", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    if 1 <= month <= 12:
+        return months[month - 1]
+    else:
+        return "error"
+
+print(number_to_month(1))
+print(number_to_month(12))
+print(number_to_month(99))
