f0b474a55322a554b9ddea422eee18d78821a139
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-10 20:15:13 -0300

    Fix formatting and return values in quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -3,12 +3,12 @@ def roots(a, b, c):
     if discriminant > 0:
         x1 = (-b + (discriminant)**0.5) / (2*a)
         x2 = (-b - (discriminant)**0.5) / (2*a)
-        return  f"({x1}, {x2})"
+        return f"({x1}, {x2})"
     elif discriminant == 0:
-        x = -b/(2*a)
+        x = -b / (2*a)
         return f"({x})"
     else:
-        return ""
+        return "( )"
 
 
 def value_y(a, b, c, x):
@@ -21,19 +21,3 @@ def to_string(a, b, c):
 
 def derivation(a, b, c):
     return f"f'(x) = {2*a}x + {b}"
-
-# Roots
-print(roots(1, -3, 2))
-print(roots(1, -2, 1))
-print(roots(1, 2, 3)) 
-
-# Value of y
-print(value_y(1, -3, 2, 0))
-print(value_y(1, -3, 2, 1))
-print(value_y(1, -3, 2, -1))
-
-# To string
-print(to_string(2, -3, 1))
-
-# Derivation
-print(derivation(2, -3, 1))
