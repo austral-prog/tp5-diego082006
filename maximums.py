771db1491b706215f1c2d0b129df36a746c7aa7a
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-10 19:42:00 -0300

    update_maximums.py

--- maximums.py
+++ maximums.py
@@ -2,22 +2,15 @@ def max_of_two(x, y):
     if x >= y:
         return x
     else:
-        y
+        return y
+    return -1
+
 
 def max_of_three(x, y, z):
     if x >= y and x >= z:
         return x
     elif y >= x and y >= z:
-        return y 
+        return y
     else:
         return z
-
-# max_of_two
-print(max_of_two(5,4))   
-print(max_of_two(-2,-3)) 
-print(max_of_two(0,0))   
-
-# max_of_three
-print(max_of_three(5,4,7))   
-print(max_of_three(-2,-3,-1)) 
-print(max_of_three(0,0,0))
+    return -1
